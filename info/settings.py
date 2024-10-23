df667ca197ca1d59700b49df1cf0bd9a441fda3a
Martin van Diest <martin.van.diest@intemo.com>
2024-10-23 09:20:28 +0200

    Try new environ settings

--- info/settings.py
+++ info/settings.py
@@ -31,7 +31,7 @@ print("path to .env : ", os.path.join(ENV_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', "CHECK THIS !!")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
