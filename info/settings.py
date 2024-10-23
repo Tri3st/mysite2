23332fdc09ec6a0a2b948942acfd959e33a31ac1
Martin van Diest <martin.van.diest@intemo.com>
2024-10-23 09:15:29 +0200

    Try new environ settings

--- info/settings.py
+++ info/settings.py
@@ -23,8 +23,10 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR.parent, '.env'))
-print("path to .env : ", os.path.join(BASE_DIR.parent, '.env'))
+ENV_DIR = BASE_DIR.parent
+
+environ.Env.read_env(os.path.join(ENV_DIR, '.env'))
+print("path to .env : ", os.path.join(ENV_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
